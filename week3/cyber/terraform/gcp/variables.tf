0838091ff3160ff562fdb9ec0bcdd2a89a2150db
Xiangyu Dong <slam.printer.08@icloud.com>
2026-04-07 17:09:14 -0700

    My Prod-Grade learning note Section 3: Azure, GCP, Data Engineering, Vectors, MCP, GCP deployment

--- week3/cyber/terraform/gcp/variables.tf
+++ week3/cyber/terraform/gcp/variables.tf
@@ -6,7 +6,7 @@ variable "project_id" {
 variable "region" {
   description = "GCP region for Cloud Run deployment"
   type        = string
-  default     = "us-central1"
+  default     = "us-west1"
 }
 
 variable "service_name" {
